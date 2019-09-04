9d6991b409bde6cf545e2bb8e9d13a653e579c17
Sean Doyle <sdoylelambda@gmail.com>
2019-09-04 16:17:17 -0400

    Validation custom middleware.

--- users/userRouter.js
+++ users/userRouter.js
@@ -33,15 +33,38 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
+    const { id } = req.params.id;
+    if(id) {
+        req.user = id;
+    } else {
+        res.status(400).json({ message: "invalid user id" })
+    }
+    next();
 };
 
 function validateUser(req, res, next) {
-
+    const { body } = req.params.body
+    if(body) {
+        res.status()
+    } else {
+        res.status(400).json({ message: "missing user data" })
+    // } else {
+    //     res.status(400).json({ message: "missing required name field" })
+    }
+    next();
 };
 
 function validatePost(req, res, next) {
+    const { body } = req.params.body
+    if(body) {
 
+    } else {
+        res.status(400).json({ message: "missing post data" })
+    }
+// } else {
+//     res.status(400).json({ message: "missing required text field" })
+// }
+    next();
 };
 
 module.exports = router;
